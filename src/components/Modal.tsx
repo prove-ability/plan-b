962c824ddbae542555cb9e9d83ede4f9d4014063
Bard <59461181+prove-ability@users.noreply.github.com>
2024-04-26 00:46:00 +0900

    refactor: modal component 개선

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,26 +1,34 @@
 "use client";
 
-import { useModalStore } from "@/stores/modal";
-import { useEffect, useRef } from "react";
+import { useRouter } from "next/navigation";
+import { PropsWithChildren } from "react";
+import { CgClose } from "react-icons/cg";
 
-export default function Modal() {
-  const state = useModalStore((state) => state.modalContents);
-  const modelEl = useRef<HTMLDialogElement>(null);
-  useEffect(() => {
-    if (!state) return;
-    modelEl.current?.showModal();
-  }, [state]);
-  if (!state) return null;
+export const Dialog = ({ children }: PropsWithChildren) => {
+  const router = useRouter();
+  const handleDialogClose = () => router.back();
   return (
-    <dialog id="modal" className="modal" ref={modelEl}>
-      <div className="modal-box">
-        <h3 className="font-bold text-lg">{state.title}</h3>
-        {state.content}
-        {/* <p className="py-4">Press ESC key or click outside to close</p> */}
+    <div
+      role="dialog"
+      className="relative z-30 bg-slate-100"
+      aria-labelledby="modal-title"
+      aria-modal="true"
+    >
+      <div
+        role="button"
+        className="fixed inset-0  bg-gray-500 bg-opacity-75 transition-opacity"
+        onClick={handleDialogClose}
+      ></div>
+      <div className="fixed flex-col inset-64 z-1 overflow-y-auto bg-white shadow-md rounded-lg">
+        <div className="flex justify-end shadow-sm p-2 sticky top-0 z-10 bg-white">
+          <button onClick={handleDialogClose} className="mb-1">
+            <div className="place-items-start">
+              <CgClose />
+            </div>
+          </button>
+        </div>
+        <div className="px-8 py-4">{children}</div>
       </div>
-      <form method="dialog" className="modal-backdrop">
-        <button>close</button>
-      </form>
-    </dialog>
+    </div>
   );
-}
+};
